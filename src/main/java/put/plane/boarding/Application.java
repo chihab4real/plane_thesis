a837fd3b3e1d37f0c50b489b0ee14bf7fc96ef12
marcinleszczynski <venkman2003@gmail.com>
2025-04-29 12:01:02 +0200

    moved to subproject

--- src/main/java/put/plane/boarding/Application.java
+++ src/main/java/put/plane/boarding/Application.java
@@ -3,6 +3,7 @@ package put.plane.boarding;
 public class Application {
 
     public static void main(String[] args) {
+        // TODO: Create optimizer
         System.out.println("hello");
     }
 }
